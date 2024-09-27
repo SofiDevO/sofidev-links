cfc3daaa4558f770d8c68413d0c632a922284044
SofiDev <angiemeow89@gmail.com>
2024-09-27 00:14:38 -0600

    ✏️ Fixing apikey

--- src/services/youtubeService.js
+++ src/services/youtubeService.js
@@ -2,7 +2,7 @@ import { YT_API_KEY } from 'astro:env/client';
 
 
 export const fetchYouTubeVideos = async (channelId) => {
-  const API_key = `${YT_API_KEY}`; //in order to get your free public API_key, you will need to create an acc. in https://rss2json.com/
+  const API_key = YT_API_KEY; //in order to get your free public API_key, you will need to create an acc. in https://rss2json.com/
   const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`);
   const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}&api_key=${API_key}`;
 
