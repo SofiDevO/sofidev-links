4f5d1066e92c51854b552ea0e1434e1f0abb20be
SofiDev <angiemeow89@gmail.com>
2024-09-26 23:33:58 -0600

    💥 Adding new features in progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,55 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://allmylinks-ten.vercel.app/",
+  site: "https://links.itssofi.dev/",
   prefetch: true,
+  output: "server",
+  // adapter: vercel(),
+  adapter: node({
+    mode: "standalone",
+  }),
+  experimental: {
+    env: {
+      schema: {
+        EMAIL_HOST: envField.string({
+          context: "server",
+          access: "secret",
+          optional: false,
+        }),
+        EMAIL: envField.string({
+          context: "server",
+          access: "secret",
+          optional: false,
+        }),
+        EMAIL_PASS: envField.string({
+          context: "server",
+          access: "secret",
+          optional: false,
+        }),
+        EMAIL_PORT: envField.number({
+          context: "server",
+          access: "secret",
+          optional: false,
+        }),
+        PUBLIC_BACKEND_URL: envField.string({
+          context: "server",
+          access: "secret",
+          optional: false,
+        }),
+        YT_API_KEY: envField.string({
+          context: "client",
+          access: "public",
+          optional: false,
+        }),
+      },
+    },
+  },
 });
