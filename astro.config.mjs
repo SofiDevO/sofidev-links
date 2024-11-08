6c7e4c17614496c3cb317fdcda1676f811f1c133
SofiDev <angiemeow89@gmail.com>
2024-11-07 18:48:41 -0600

    ♻️ Se elimina codigo innecesario

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,31 +16,6 @@ export default defineConfig({
   experimental: {
     env: {
       schema: {
-        EMAIL_HOST: envField.string({
-          context: "server",
-          access: "secret",
-          optional: false,
-        }),
-        EMAIL: envField.string({
-          context: "server",
-          access: "secret",
-          optional: false,
-        }),
-        EMAIL_PASS: envField.string({
-          context: "server",
-          access: "secret",
-          optional: false,
-        }),
-        EMAIL_PORT: envField.number({
-          context: "server",
-          access: "secret",
-          optional: false,
-        }),
-        PUBLIC_BACKEND_URL: envField.string({
-          context: "server",
-          access: "secret",
-          optional: false,
-        }),
         YT_API_KEY: envField.string({
           context: "client",
           access: "public",
