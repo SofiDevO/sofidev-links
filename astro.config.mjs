d7492b3a898d3372c9e5339daa95373b5cfe2c59
SofiDevO <angiemeow89@gmail.com>
2025-01-22 08:39:41 -0600

    ♻️ Se eliminan dependencias Udate Astro
    - Se eliminan dependecias innecesarias
    - Se cambia ssr por sitio estático
    - Se actualiza Astro por lo que la variable de entorno ya no es experimental

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,20 @@
-import { defineConfig,envField } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import react from "@astrojs/react";
 
-import node from "@astrojs/node";
 
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [],
   site: "https://links.itssofi.dev/",
-  prefetch: true,
-  output: "server",
-  adapter: vercel(),
-  experimental: {
-    env: {
-      schema: {
-        YT_API_KEY: envField.string({
-          context: "client",
-          access: "public",
-          optional: false,
-        }),
-      },
+
+  env: {
+    schema: {
+      YT_API_KEY: envField.string({
+        context: "client",
+        access: "public",
+        optional: false,
+      }),
     },
   },
 });
